2f201fdd8b8ea23ba62f44be6f44346309cb1ac1
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-04-08 18:50:05 +0600

    Login with many data

--- WebsiteTest/src/test/java/PracticeProject/HomePage.java
+++ WebsiteTest/src/test/java/PracticeProject/HomePage.java
@@ -2,19 +2,46 @@ package PracticeProject;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
+import pageObjects.LoginPage;
 import resources.Base;
 
 public class HomePage extends Base{
 
-	@Test
-	public void basePageNavigation() throws IOException
+	@Test(dataProvider="getData")
+	public void basePageNavigation(String username, String password, String text) throws IOException
 	{
 		driver =InitializeDriver();
-		driver.get("http://www.qaclickacademy.com/");
+		driver.get("http://www.qaclickacademy.com/"); //Data provider annotation
 		LandingPage land =new LandingPage(driver);
 		land.SignIn().click();
+		LoginPage log =new LoginPage(driver);
+		log.getEmail().sendKeys(username);
+		log.getPass().sendKeys(password);
+		log.loginBtn().click();
+		System.out.println(text);
+	}
+	
+	@DataProvider
+	public Object[][] getData()
+	{
+		//column means how many types of data like restricted/ not restricted
+		//row stands for how many different data types test should run
+		//like username,pass,text etc
+		//Object[][] data=new Object[1][2]; //array index out of bound 
+		Object[][] data=new Object[2][3];
+		
+		data[0][0]="suparnashil@gmail.com";
+		data[0][1]="1234567";
+		data[0][2]="Restricted";
+	    
+		data[1][0]="chaity@gmail.com";
+		data[1][1]="11111";
+		data[1][2]="Not Restricted"; 
+		
+		return data;
 	}
 }
--- WebsiteTest/src/test/java/pageObjects/LoginPage.java
+++ WebsiteTest/src/test/java/pageObjects/LoginPage.java
@@ -1,5 +1,34 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
 
+public WebDriver driver;
+	
+	By email =By.xpath("//input[@id='user_email']");
+	By pass =By.xpath("//input[@id='user_password']");
+	By loginButton =By.cssSelector("[value='Log In']");
+	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	public WebElement getEmail()
+	{
+		return driver.findElement(email);
+	}
+	
+	public WebElement getPass()
+	{
+		return driver.findElement(pass);
+	}
+	
+	public WebElement loginBtn()
+	{
+		return driver.findElement(loginButton);
+	}
 }
